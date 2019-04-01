d9e21925fb43d5c838a455c467d30e7cbb862f39
martin <mdtn@gmx.de>
2019-04-01 11:51:17 +0200

    first commit

---
+++ brutus/brutus.py
@@ -0,0 +1,11 @@
+from lib.Initiate import Initiate
+import configparser
+
+
+if __name__ == '__main__':
+
+    # Harvest files in ../tests recursively, use definitions.json as configuration file
+    # and current folder as "contents path"
+    init = Initiate("paths.cfg")
+    # Create disk image folder in upper directory
+    init.start_sampler()
