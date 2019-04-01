f92d63e69a8aed695636a92340fb538f3ac6a245
martin <mdtn@gmx.de>
2019-04-01 13:36:04 +0200

    Removed unnecessary comments

--- brutus/brutus.py
+++ brutus/brutus.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
 
     # Harvest files in ../tests recursively, use definitions.json as configuration file
     # and current folder as "contents path"
-    init = Initiate("paths.cfg")
+    init = Initiate("configuration.cfg")
     # Create disk image folder in upper directory
     init.start_sampler()
